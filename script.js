249b42b8282f724e2860c518c9cecbe6289b9f88
vijayB <88872426+vijay451@users.noreply.github.com>
2021-09-15 11:12:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+var form =document.getElementById("form");
+var userName =document.getElementById("userName");
+var email=document.getElementById("Email");
+var currentPassword= document.getElementById("currentPassword");
+var newPassword= document.getElementById("newPassword");
+
+function showErrorMessage(input,message){
+    const formControl=input.parentElement;
+    formControl.className="form-control error";
+    const errorElem=formControl.querySelector("small");
+    errorElem.innerText=message;
+}
+
+function showSuccessMessage(input){
+    const formControl=input.parentElement;
+    formControl.className="form-control success";
+    const errorElem=formControl.querySelector("small");
+    errorElem.innerText="";
+
+}
+
+
+function checkRequiredField(inputs){
+    for(var i=0;i<inputs.length;i++){
+        var input=inputs[i];
+        if(input.value===""){
+            const message=input.id+" is required";
+            showErrorMessage(input,message);
+        }
+        else{
+            showSuccessMessage(input);
+        }
+
+    }
+}
+
+
+
+form.addEventListener('submit',function(event){
+    event.preventDefault();
+    checkRequiredField([userName,email,currentPassword,newPassword]);
+
+})
